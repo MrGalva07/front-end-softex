60e77b8e40817ad4833a8dcc8f7adfc99b526a82
MrGalva07 <lucas.galvao0707@gmail.com>
2024-08-10 15:49:08 -0300

    while

---
+++ src/atividade_06/q02.ts
@@ -0,0 +1,5 @@
+/* Descreva em quais circunstâncias os comandos "for" e "while" devem ser empregados.*/
+
+/*RESPOSTA:
+FOR -> deve ser empregado quando se sabe o número exato de iterações que deseja executar.
+WHILE ->deve ser empregado quando não se sabe o número exato de iterações que deseja executar.*/
---
+++ src/atividade_06/q09.ts
@@ -0,0 +1,13 @@
+/* Faça um programa que calcule o produto dos números de 1 a 5 utilizando o while.*/
+
+let coun:number=1
+let produto:number = 1
+
+while(coun<=5){
+
+    produto = coun * produto
+    coun++
+  
+}
+
+console.log(produto)
